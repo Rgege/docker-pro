945277d4b12ba8b7ce002eda67011cef86ed869d
m15801983447@163.com <xrln47928>
2019-04-18 21:08:57 +0800

    dockering

--- white-mouse/src/main/java/com/xrdocker/whitemouse/controller/TestController.java
+++ white-mouse/src/main/java/com/xrdocker/whitemouse/controller/TestController.java
@@ -17,6 +17,7 @@ package com.xrdocker.whitemouse.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -31,7 +32,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/test/")
 public class TestController {
 
-    @RequestMapping("sayHello")
+    @GetMapping("sayHello")
     @ResponseBody
     public String sayHello(){
         return "hello ~~~~~~~~";
