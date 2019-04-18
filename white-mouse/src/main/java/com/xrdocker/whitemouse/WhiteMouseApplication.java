abd3987b731dba3d9c3a6878a6f46e75014a91a3
m15801983447@163.com <xrln47928>
2019-04-18 17:21:24 +0800

    create mouse project

---
+++ white-mouse/src/main/java/com/xrdocker/whitemouse/WhiteMouseApplication.java
@@ -0,0 +1,13 @@
+package com.xrdocker.whitemouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteMouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteMouseApplication.class, args);
+    }
+
+}
---
+++ white-mouse/src/test/java/com/xrdocker/whitemouse/WhiteMouseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xrdocker.whitemouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiteMouseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
