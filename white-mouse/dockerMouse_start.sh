88c89fb2058e796e2b2a511a487b3b54dbdfdf26
m15801983447@163.com <xrln47928>
2019-04-22 17:47:55 +0800

    common commit

--- white-mouse/dockerMouse_start.sh
+++ white-mouse/dockerMouse_start.sh
@@ -20,3 +20,5 @@ appname="$1"
 #/usr/lib/dev/maven/apache-maven-3.6.1/bin/mvn clean install -P prd dockerfile:build
 echo "====================start docker container:["${appname}"]===================="
 /usr/bin/docker run -d -p 8001:8001 --name $appname $appname
+#删除none镜像
+/usr/bin/docker rmi $(docker images -f "dangling=true" -q)
