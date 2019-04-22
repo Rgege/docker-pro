560749b8740882d707a5bf454a16557dad292ce1
m15801983447@163.com <xrln47928>
2019-04-22 15:08:32 +0800

    shell modify

--- white-mouse/dockerMouse_start.sh
+++ white-mouse/dockerMouse_start.sh
@@ -1,22 +1,21 @@
 appname="dkmouse01"
-containerName="1"
-echo "$containerName"
+word="1"
 #containerName=$(docker ps -a -q --no-trunc --filter name= $appName)
-containerName=`docker ps -a -q --no-trunc --filter name= $appName`
-echo "=========================exit:$containerName"
-if [ -z "$containerName" ];
+word=`docker ps -a -q --no-trunc --filter name=^/"$appName"$`
+echo "=========================exit:$word"
+if [ -z "$word" ];
 then
     echo "====================当前不存在该容器，直接进行启动该操作===================="
-elif [ -n "$containerName" ];
+elif [ -n "$word" ];
 then
     echo "====================当前已存在容器，停止并移除该容器===================="
-    /usr/bin/docker stop "$containerName"
-    /usr/bin/docker rm "$containerName"
-elif [ "$containerName" == "1" ];
+    /usr/bin/docker stop "$word"
+    /usr/bin/docker rm "$word"
+elif [ "$word" == "1" ];
 then
     echo "====================查询的信息有误，执行默认操作===================="
-    /usr/bin/docker stop "$containerName"
-    /usr/bin/docker rm "$containerName"
+    /usr/bin/docker stop "$word"
+    /usr/bin/docker rm "$word"
 fi
 #这里注释掉  应为Jenkins构建命令已经有过一次打包行为  所以注掉
 #/usr/lib/dev/maven/apache-maven-3.6.1/bin/mvn clean install -P prd dockerfile:build
