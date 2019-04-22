4fb3766ee9b72e8bc8a11b30b7605c09b7e7e158
m15801983447@163.com <xrln47928>
2019-04-22 14:15:05 +0800

    common commit

--- white-mouse/dockerMouse_start.sh
+++ white-mouse/dockerMouse_start.sh
@@ -1,21 +1,21 @@
 appname="dkmouse01"
-word="1"
-echo "$word"
-word=`docker ps -a -q --no-trunc --filter name=^/"$appName"$`
-echo "$word"
-if [ -z "$word" ];
+containerName="1"
+echo "$containerName"
+containerName=`docker ps -a -q --no-trunc --filter name=^/"$appName"$`
+echo "$containerName"
+if [ -z "$containerName" ];
 then
     echo "====================当前不存在该容器，直接进行启动该操作===================="
-elif [ -n "$word" ];
+elif [ -n "$containerName" ];
 then
     echo "====================当前已存在容器，停止并移除该容器===================="
-    /usr/bin/docker stop "$word"
-    /usr/bin/docker rm "$word"
-elif [ "$word" == "1" ];
+    /usr/bin/docker stop "$containerName"
+    /usr/bin/docker rm "$containerName"
+elif [ "$containerName" == "1" ];
 then
     echo "====================查询的信息有误，执行默认操作===================="
-    /usr/bin/docker stop "$word"
-    /usr/bin/docker rm "$word"
+    /usr/bin/docker stop "$containerName"
+    /usr/bin/docker rm "$containerName"
 fi
-/usr/lib/dev/maven/apache-maven-3.6.1/bin/mvn clean install -P prd dockerfile:build
+#/usr/lib/dev/maven/apache-maven-3.6.1/bin/mvn clean install -P prd dockerfile:build
 /usr/bin/docker run -d -p 8001:8001 --name $appname $appname
