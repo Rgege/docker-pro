201231d5e4b8e58961c60298231f29ac9b177937
m15801983447@163.com <xrln47928>
2019-04-22 14:46:20 +0800

    shell modify

--- white-mouse/dockerMouse_start.sh
+++ white-mouse/dockerMouse_start.sh
@@ -1,7 +1,7 @@
 appname="dkmouse01"
 containerName="1"
 echo "$containerName"
-containerName=`docker ps -a -q --no-trunc --filter name=^/"$appName"$`
+containerName=$(docker ps -a -q --no-trunc --filter name=^/"$appName")
 echo "$containerName"
 if [ -z "$containerName" ];
 then
@@ -17,5 +17,6 @@ then
     /usr/bin/docker stop "$containerName"
     /usr/bin/docker rm "$containerName"
 fi
+#这里注释掉  应为Jenkins构建命令已经有过一次打包行为  所以注掉
 #/usr/lib/dev/maven/apache-maven-3.6.1/bin/mvn clean install -P prd dockerfile:build
 /usr/bin/docker run -d -p 8001:8001 --name $appname $appname
