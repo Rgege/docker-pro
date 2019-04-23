fe720ad1e8b080adb3452dc3efb244059b1961d3
m15801983447@163.com <xrln47928>
2019-04-23 10:43:18 +0800

    common commit

--- white-mouse/dockerMouse_start.sh
+++ white-mouse/dockerMouse_start.sh
@@ -1,8 +1,11 @@
 appname="$1"
-javaOPTS="-Xms512m -Xmx512m -Djava.rmi.server.hostname=211.149.142.55 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.rmi.port=11199 -Dcom.sun.management.jmxremote.port=1199 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+javaOPTS="-Xms512m -Xmx512m -Djava.rmi.server.hostname=211.149.142.55 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.rmi.port=8999 -Dcom.sun.management.jmxremote.port=8999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 
 echo "====================start docker container:["${appname}"]===================="
 #/usr/bin/docker run -d -p 8001:8001 --name $appname $appname
 /usr/bin/docker run -d -e JAVA_OPTS=$javaOPTS -p 8001:8001 --name $appname $appname
 #删除none镜像
 /usr/bin/docker rmi $(docker images -f "dangling=true" -q)
+
+echo "====================映射端口===================="
+/usr/bin/docker -p 8999:8999
