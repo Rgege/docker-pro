1b17239555ad1b43e58031205c38b8847bfe3b45
m15801983447@163.com <xrln47928>
2019-04-23 10:22:57 +0800

    common commit

--- white-mouse/dockerMouse_start.sh
+++ white-mouse/dockerMouse_start.sh
@@ -1,24 +1,8 @@
 appname="$1"
-#word="1"
-#word=$(docker ps -a -q --no-trunc --filter name=${appname})
-#echo "=========================exit:$word"
-#if [ -z "$word" ];
-#then
-#    echo "====================当前不存在该容器，直接进行启动该操作===================="
-#elif [ -n "$word" ];
-#then
-#    echo "====================当前已存在容器，停止并移除该容器===================="
-#    /usr/bin/docker stop "$word"
-#    /usr/bin/docker rm "$word"
-#elif [ "$word" == "1" ];
-#then
-#    echo "====================查询的信息有误，执行默认操作===================="
-#    /usr/bin/docker stop "$word"
-#    /usr/bin/docker rm "$word"
-#fi
-#这里注释掉  应为Jenkins构建命令已经有过一次打包行为  所以注掉
-#/usr/lib/dev/maven/apache-maven-3.6.1/bin/mvn clean install -P prd dockerfile:build
+javaOPTS="$2"
+
 echo "====================start docker container:["${appname}"]===================="
-/usr/bin/docker run -d -p 8001:8001 --name $appname $appname
+#/usr/bin/docker run -d -p 8001:8001 --name $appname $appname
+/usr/bin/docker run -d -e JAVA_OPTS=$javaOPTS -p 8001:8001 --name $appname $appname
 #删除none镜像
 /usr/bin/docker rmi $(docker images -f "dangling=true" -q)
