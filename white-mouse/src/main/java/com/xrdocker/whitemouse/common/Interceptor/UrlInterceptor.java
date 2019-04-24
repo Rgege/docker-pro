57f26eea7dd87ba7b92e6fddf82f6605047ad844
m15801983447@163.com <xrln47928>
2019-04-24 22:41:31 +0800

    add common project for public resource data

--- white-mouse/src/main/java/com/xrdocker/whitemouse/common/Interceptor/UrlInterceptor.java
+++ white-mouse/src/main/java/com/xrdocker/whitemouse/common/Interceptor/UrlInterceptor.java
@@ -1,5 +1,6 @@
 package com.xrdocker.whitemouse.common.Interceptor;
 
+import com.xrdocker.whitemouse.util.NetworkUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -14,13 +15,15 @@ public class UrlInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
         StringBuffer url=request.getRequestURL();
+        String ip=NetworkUtil.getClientIpAddr(request);
+        LOGGER.debug("===========================================================this reqIp:"+ NetworkUtil.getClientIpAddr(request));
         LOGGER.debug("===========================================================this url:"+url);
 
         if (url.indexOf("/wmiuc/")!=-1) {
             return true;
         }else {
             // 跳转登录-------controller类中的登陆方法
-            String tourl = "https://www.baidu.com/s?wd=杀人";
+            String tourl = "https://www.baidu.com/s?wd=ip";
             response.sendRedirect(tourl);
             return false;
         }
---
+++ white-mouse/src/main/java/com/xrdocker/whitemouse/util/NetworkUtil.java
@@ -0,0 +1,84 @@
+package com.xrdocker.whitemouse.util;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * 常用获取客户端信息的工具
+ *
+ */
+public final class NetworkUtil {
+
+    /**
+     * 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址;
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    public final static String getClientIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-forwarded-for");
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0
+                    || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0
+                    || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0
+                    || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (ip == null || ip.length() == 0
+                    || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (ip == null || ip.length() == 0
+                    || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+            }
+        } else if (ip.length() > 15) {
+            String[] ips = ip.split(",");
+            for (int index = 0; index < ips.length; index++) {
+                String strIp = (String) ips[index];
+                if (!("unknown".equalsIgnoreCase(strIp))) {
+                    ip = strIp;
+                    break;
+                }
+            }
+        }
+        return ip;
+    }
+
+    /**
+     * 获取serverIP地址
+     *
+     * @param
+     * @return
+     * @throws IOException
+     */
+    public final static String getServerIpAddr() {
+        StringBuilder inconfig=new StringBuilder();
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress() && inetAddress.isSiteLocalAddress()) {
+                        inconfig.append(inetAddress.getHostAddress().toString()+" ");
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return inconfig.toString().trim();
+    }
+}
