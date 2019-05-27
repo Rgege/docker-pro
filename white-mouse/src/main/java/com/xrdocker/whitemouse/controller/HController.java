e5862aefaae8125e8ae8f2b5269dd7728c353a19
m15801983447@163.com <xrln47928>
2019-05-27 19:57:39 +0800

    add static source

--- white-mouse/src/main/java/com/xrdocker/whitemouse/controller/HController.java
+++ white-mouse/src/main/java/com/xrdocker/whitemouse/controller/HController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/H/")
 public class HController {
@@ -18,5 +20,22 @@ public class HController {
     @ResponseBody
     public String sayHello(){
         return "hello ~~~~~~~~";
+    }
+
+    @GetMapping("jstack")
+    @ResponseBody
+    public String jstack(){
+        for (Map.Entry<Thread, StackTraceElement[]> stackTrace : Thread.getAllStackTraces().entrySet()) {
+            Thread thread = (Thread) stackTrace.getKey();
+            StackTraceElement[] stack = (StackTraceElement[]) stackTrace.getValue();
+            if (thread.equals(Thread.currentThread())) {
+                continue;
+            }
+            System.out.println("\n线程：" + thread.getName() + "\n");
+            for (StackTraceElement element : stack) {
+                System.out.println("\t" + element + "\n");
+            }
+        }
+        return "1";
     }
 }
